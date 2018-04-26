6367650d415c54e29e213c53b11ed0ecae3cc515
Emily Jirles <emily.jirles@gmail.com>
2018-04-26 20:00:48 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,7 +23,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if /(\d{10}|\d{3}\s\d{3}\s\d{4}|\(\d{3}\)\d{3}-\d{4}|\d{3}-\d{3}-\d{4})/.match(phone)
+  if /\b(\d{10}|\d{3}\s\d{3}\s\d{4}|\(\d{3}\)\d{3}-\d{4}|\d{3}-\d{3}-\d{4})\b/.match(phone)
     true 
   else
     false
