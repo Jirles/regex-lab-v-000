b330d4f097bb0a9cf7cdab430505263250690be7
Emily Jirles <emily.jirles@gmail.com>
2018-04-26 18:24:24 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,17 @@
 def starts_with_a_vowel?(word)
-
+  if /\b[AaEeIiOoUu]/.match(word)
+    true 
+  else 
+    false 
+  end 
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\bun+.+$\ing/)
 end
 
 def words_five_letters_long(text)
-
+  /^\w{5}\s/.match(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
